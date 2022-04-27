e14af26304531cdcb47982ad06470d0694ee852f
sheng--qing <1406707496@qq.com>
2022-04-27 10:26:00 +0800

    归并有序数组（双指针）

---
+++ Array/88_Merge_Sorted_Array.cpp
@@ -0,0 +1,15 @@
+/*归并两个有序数组*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int pos = m-- + n-- - 1;
+    while (m >= 0 && n >= 0) {
+        nums1[pos--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+    }
+    while (n >= 0) {
+        nums1[pos--] = nums2[n--];
+    }
+}
